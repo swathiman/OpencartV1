2eb1fdb9362497a2af3e4eb07a53efcd0ef61f51
swathi <manne.swathi@gmail.com>
2025-03-05 22:32:15 -0500

    fourth commit

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -3,6 +3,7 @@ package pageObjects;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -13,11 +14,10 @@ public class HomePage extends BasePage{
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
-	@FindBy(xpath ="//span[normalize-space()='My Account']")
+	@FindBy(xpath = "//div[@id='top-links']/ul/li[2]/a")
 	WebElement linkMyAccount;
 	
 	
-	//((JavascriptExecutor) driver).executeScript("document.getElementById('ID').style.display='block';");
 	@FindBy(xpath ="//a[text()='Register']")
 	WebElement linkRegister;
 	
@@ -28,25 +28,19 @@ public class HomePage extends BasePage{
 	
 	public void clickOnMyAccount()
 	{
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[normalize-space()='My Account']")));
-		element.click();
-
-		//linkMyAccount.click();
+		
+		
+		linkMyAccount.click();
 	}
 	public void clickOnRegister()
 	{
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Register']")));
-		element.click();
-		//linkRegister.click();
+		
+		linkRegister.click();
 	}
 	public void clickOnLogin()
 	{
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Login']")));
-		element.click();
-		//linkLogin.click();
+		
+		linkLogin.click();
 	}
 	public void enterProductName(String pName)   //For Search Product Test
 	{
