cc04fb631dd4e1550b0b0dd7a7de94ee1bd24bfd
swathi <manne.swathi@gmail.com>
2025-03-05 21:58:57 -0500

    third commit

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,8 +1,13 @@
 package pageObjects;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends BasePage{
 	public HomePage(WebDriver driver) {
@@ -10,6 +15,9 @@ public class HomePage extends BasePage{
 	}
 	@FindBy(xpath ="//span[normalize-space()='My Account']")
 	WebElement linkMyAccount;
+	
+	
+	//((JavascriptExecutor) driver).executeScript("document.getElementById('ID').style.display='block';");
 	@FindBy(xpath ="//a[text()='Register']")
 	WebElement linkRegister;
 	
@@ -18,18 +26,27 @@ public class HomePage extends BasePage{
 	@FindBy(xpath ="//input[@name='search']")
 	WebElement searchBox;
 	
-	
 	public void clickOnMyAccount()
 	{
-		linkMyAccount.click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[normalize-space()='My Account']")));
+		element.click();
+
+		//linkMyAccount.click();
 	}
 	public void clickOnRegister()
 	{
-		linkRegister.click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Register']")));
+		element.click();
+		//linkRegister.click();
 	}
 	public void clickOnLogin()
 	{
-		linkLogin.click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Login']")));
+		element.click();
+		//linkLogin.click();
 	}
 	public void enterProductName(String pName)   //For Search Product Test
 	{
